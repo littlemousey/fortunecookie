219716328803dd44f4ee2e54a497f0eb6cd72b9a
Ans de Nijs <ans@chordify.net>
2025-02-02 23:37:46 +0100

    Feat: Add Lunar background music

--- preact-fortune-cookie/src/components/Header.tsx
+++ preact-fortune-cookie/src/components/Header.tsx
@@ -1,7 +1,30 @@
 import { useLocation } from 'preact-iso';
+import { useReducer } from 'preact/hooks';
+import sound from '../assets/background-lunar-new-year.mp3';
+
+const backgroundMusic = new Audio(sound);
 
 export function Header() {
 	const { url } = useLocation();
+	const [toggle, setToggle] = useReducer(() => !toggle, false);
+
+	const handleMusicToggle = () => {
+		if (!toggle) {
+			setToggle(!toggle);
+			return;
+		}
+
+		setToggle(!toggle);
+	}
+
+	const musicOff = () => {
+		backgroundMusic.pause();
+		backgroundMusic.currentTime = 0;
+	}
+
+	const musicOn = () => {
+		backgroundMusic.play();
+	}
 
 	return (
 		<header>
@@ -12,6 +35,10 @@ export function Header() {
 				<a href="/404" class={url == '/404' && 'active'}>
 					404
 				</a>
+				<span class="icon" onClick={handleMusicToggle}>
+					{!toggle && <span onClick={musicOn}>🔊</span>}
+					{toggle && <span onClick={musicOff}>🔇</span>}
+				</span>
 			</nav>
 		</header>
 	);
