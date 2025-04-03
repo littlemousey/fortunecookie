696c2c937b191f9849b84e1e3c52e956da01c7f5
Ans de Nijs <ans@chordify.net>
2025-04-03 23:41:30 +0200

    Feat: Add current year and time until next year

--- preact-fortune-cookie/src/pages/_lunaryear.tsx
+++ preact-fortune-cookie/src/pages/_lunaryear.tsx
@@ -1,44 +1,62 @@
-import type { FireworksHandlers } from '@fireworks-js/preact';
-import { Fireworks } from '@fireworks-js/preact';
-import { useRef } from 'preact/hooks';
 import '../style.css';
 
 export function LunarYear() {
-	const ref = useRef<FireworksHandlers>(null)
+	const currentDate = new Date;
+	const currentYear = currentDate.getFullYear();
 
-	const toggleFireworks = () => {
-		if (!ref.current) return
-		if (ref.current.isRunning) {
-		  ref.current.stop()
-		} else {
-		  ref.current.start()
+	const calculateCurrentZodiac = (year: number) => {
+		const animals = [
+			"Rat 🐀",
+			"Ox 🐂",
+			"Tiger 🐅",
+			"Rabbit 🐇",
+			"Dragon 🐉",
+			"Snake 🐍",
+			"Horse 🐎",
+			"Goat 🐐",
+			"Monkey 🐒",
+			"Rooster 🐓",
+			"Dog 🐕",
+			"Pig 🐖"
+		];
+		const index = (year - 4) % 12;
+
+		return animals[index];
+	}
+
+	function daysUntilChineseNewYear() {
+		const chineseNewYears = [
+			new Date(2025, 2, 29),
+			new Date(2026, 2, 17),
+			new Date(2027, 2, 6),
+			new Date(2028, 1, 26),
+		];
+		const today = new Date();
+	
+		// If CNY has already passed this year, calculate for next year
+		let count = 0;
+		while (today > chineseNewYears[count]) {
+			count++;
 		}
-	  }
+
+		const nextCNY = chineseNewYears[count];
+		// Calculate the difference in days
+		const diffTime = nextCNY.getTime() - today.getTime();
+
+		return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	}
 
 	return (
 		<section>
 			<h1>What is New Lunar Year about?</h1>
-			<p>Chinese New Year. It is about being with family and friends and having good food together.</p>
-
+			<p>Lunar New Year is also known as Chinese New Year or Spring Festival (春节).
+				It is the most important festival in China and other Asian countries.
+				As the name suggests, it marks the beginning of the lunar calendar and is celebrated with family gatherings.
+			</p>
+			<h2>What is the current year?</h2>
+			<div>{calculateCurrentZodiac(currentYear)}</div>
 			<h2>When is the next Lunar New Year?</h2>
-			<p>days until the new lunar year</p>
-			<Fireworks
-			autostart={false}
-			ref={ref}
-			options={{ opacity: 0.5 }}
-			style={{
-				bottom: 40,
-				left: 200,
-				width: '50%',
-				height: '50%',
-				position: 'absolute',
-				background: '#000'
-			}}
-			/>
-			<div style={{ display: 'flex', gap: '4px', position: 'absolute', zIndex: 1 }} >
-				<button onClick={() => toggleFireworks()}>Toggle</button>
-				<button onClick={() => ref.current!.clear()}>Clear</button>
-			</div>
+			<p>{daysUntilChineseNewYear()} days until the new lunar year</p>
 		</section>
 	)
 }
